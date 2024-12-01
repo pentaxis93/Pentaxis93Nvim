8351aeef80ea526b6678721581c8924aad9c6600
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2024-12-01 10:02:48 +0100

    feat: load supermaven plugin eagerly

--- lua/plugins/4-dev.lua
+++ lua/plugins/4-dev.lua
@@ -205,11 +205,11 @@ return {
         callback = function()
           local is_aerial = vim.bo.filetype == "aerial"
           local is_ufo_available =
-              require("base.utils").is_available("nvim-ufo")
+            require("base.utils").is_available("nvim-ufo")
           if
-              is_ufo_available
-              and is_aerial
-              and vim.b.new_aerial_session == nil
+            is_ufo_available
+            and is_aerial
+            and vim.b.new_aerial_session == nil
           then
             vim.b.new_aerial_session = false
             require("aerial").tree_set_collapse_level(0, 0)
@@ -323,6 +323,7 @@ return {
   {
     "supermaven-inc/supermaven-nvim",
     config = function() require("supermaven-nvim").setup({}) end,
+    lazy = false,
   },
   --  neural [chatgpt code generator]
   --  https://github.com/dense-analysis/neural
@@ -478,7 +479,7 @@ return {
       dap.adapters.python = {
         type = "executable",
         command = vim.fn.stdpath("data")
-            .. "/mason/packages/debugpy/venv/bin/python",
+          .. "/mason/packages/debugpy/venv/bin/python",
         args = { "-m", "debugpy.adapter" },
       }
       dap.configurations.python = {
@@ -563,13 +564,13 @@ return {
           initCommands = function() -- add rust types support (optional)
             -- Find out where to look for the pretty printer Python module
             local rustc_sysroot =
-                vim.fn.trim(vim.fn.system("rustc --print sysroot"))
+              vim.fn.trim(vim.fn.system("rustc --print sysroot"))
 
             local script_import = 'command script import "'
-                .. rustc_sysroot
-                .. '/lib/rustlib/etc/lldb_lookup.py"'
+              .. rustc_sysroot
+              .. '/lib/rustlib/etc/lldb_lookup.py"'
             local commands_file = rustc_sysroot
-                .. "/lib/rustlib/etc/lldb_commands"
+              .. "/lib/rustlib/etc/lldb_commands"
 
             local commands = {}
             local file = io.open(commands_file, "r")
@@ -631,8 +632,8 @@ return {
           request = "launch",
           name = "Launch dart",
           dartSdkPath = "/opt/flutter/bin/cache/dart-sdk/", -- ensure this is correct
-          flutterSdkPath = "/opt/flutter",                  -- ensure this is correct
-          program = "${workspaceFolder}/lib/main.dart",     -- ensure this is correct
+          flutterSdkPath = "/opt/flutter", -- ensure this is correct
+          program = "${workspaceFolder}/lib/main.dart", -- ensure this is correct
           cwd = "${workspaceFolder}",
         },
         {
@@ -640,8 +641,8 @@ return {
           request = "launch",
           name = "Launch flutter",
           dartSdkPath = "/opt/flutter/bin/cache/dart-sdk/", -- ensure this is correct
-          flutterSdkPath = "/opt/flutter",                  -- ensure this is correct
-          program = "${workspaceFolder}/lib/main.dart",     -- ensure this is correct
+          flutterSdkPath = "/opt/flutter", -- ensure this is correct
+          program = "${workspaceFolder}/lib/main.dart", -- ensure this is correct
           cwd = "${workspaceFolder}",
         },
       }
@@ -659,7 +660,7 @@ return {
           request = "launch",
           name = "Launch kotlin program",
           projectRoot = "${workspaceFolder}/app", -- ensure this is correct
-          mainClass = "AppKt",                    -- ensure this is correct
+          mainClass = "AppKt", -- ensure this is correct
         },
       }
 
@@ -728,7 +729,7 @@ return {
       dap.adapters.bashdb = {
         type = "executable",
         command = vim.fn.stdpath("data")
-            .. "/mason/packages/bash-debug-adapter/bash-debug-adapter",
+          .. "/mason/packages/bash-debug-adapter/bash-debug-adapter",
         name = "bashdb",
       }
       dap.configurations.sh = {
@@ -738,9 +739,9 @@ return {
           name = "Launch file",
           showDebugOutput = true,
           pathBashdb = vim.fn.stdpath("data")
-              .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb",
+            .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb",
           pathBashdbLib = vim.fn.stdpath("data")
-              .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir",
+            .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir",
           trace = true,
           file = "${file}",
           program = "${file}",
@@ -884,10 +885,10 @@ return {
         virtual_text = {
           format = function(diagnostic)
             local message = diagnostic.message
-                :gsub("\n", " ")
-                :gsub("\t", " ")
-                :gsub("%s+", " ")
-                :gsub("^%s+", "")
+              :gsub("\n", " ")
+              :gsub("\t", " ")
+              :gsub("%s+", " ")
+              :gsub("^%s+", "")
             return message
           end,
         },
