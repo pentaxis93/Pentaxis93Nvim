1499d89e38e53ed5f10a3beb5bbb2661ba3ba06d
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2024-12-09 12:07:51 +0100

    feat: enable automatic flutter hot reloading

--- lua/base/3-autocmds.lua
+++ lua/base/3-autocmds.lua
@@ -34,19 +34,24 @@ local is_available = utils.is_available
 autocmd({ "BufReadPost", "BufNewFile", "BufWritePost" }, {
   desc = "Nvim user events for file detection (BaseFile and BaseGitFile)",
   callback = function(args)
-    local empty_buffer = vim.fn.resolve(vim.fn.expand "%") == ""
-    local greeter = vim.api.nvim_get_option_value("filetype", { buf = args.buf }) == "alpha"
-    local git_repo = utils.run_cmd(
-    { "git", "-C", vim.fn.fnamemodify(vim.fn.resolve(vim.fn.expand "%"), ":p:h"), "rev-parse" }, false)
+    local empty_buffer = vim.fn.resolve(vim.fn.expand("%")) == ""
+    local greeter = vim.api.nvim_get_option_value(
+      "filetype",
+      { buf = args.buf }
+    ) == "alpha"
+    local git_repo = utils.run_cmd({
+      "git",
+      "-C",
+      vim.fn.fnamemodify(vim.fn.resolve(vim.fn.expand("%")), ":p:h"),
+      "rev-parse",
+    }, false)
 
     -- For any file exept empty buffer, or the greeter (alpha)
     if not (empty_buffer or greeter) then
       utils.trigger_event("User BaseFile")
 
       -- Is the buffer part of a git repo?
-      if git_repo then
-        utils.trigger_event("User BaseGitFile")
-      end
+      if git_repo then utils.trigger_event("User BaseGitFile") end
     end
   end,
 })
@@ -59,9 +64,7 @@ autocmd({ "VimEnter" }, {
       utils.trigger_event("User BaseDefered", true)
       utils.trigger_event("BufEnter", true) -- also, initialize tabline_buffers.
     else                                    -- Wait some ms before triggering the event.
-      vim.defer_fn(function()
-        utils.trigger_event("User BaseDefered")
-      end, 70)
+      vim.defer_fn(function() utils.trigger_event("User BaseDefered") end, 70)
     end
   end,
 })
@@ -71,7 +74,7 @@ autocmd({ "BufWinLeave", "BufWritePost", "WinLeave" }, {
   desc = "Save view with mkview for real files",
   callback = function(args)
     if vim.b[args.buf].view_activated then
-      vim.cmd.mkview { mods = { emsg_silent = true } }
+      vim.cmd.mkview({ mods = { emsg_silent = true } })
     end
   end,
 })
@@ -91,28 +94,34 @@ autocmd("BufWinEnter", {
           and not vim.tbl_contains(ignore_filetypes, filetype)
       then
         vim.b[args.buf].view_activated = true
-        vim.cmd.loadview { mods = { emsg_silent = true } }
+        vim.cmd.loadview({ mods = { emsg_silent = true } })
       end
     end
   end,
 })
 
 -- 3. Launch alpha greeter on startup
-if is_available "alpha-nvim" then
+if is_available("alpha-nvim") then
   autocmd({ "User", "BufEnter" }, {
     desc = "Disable status and tablines for alpha",
     callback = function(args)
       local is_filetype_alpha = vim.api.nvim_get_option_value(
-        "filetype", { buf = 0 }) == "alpha"
+        "filetype",
+        { buf = 0 }
+      ) == "alpha"
       local is_empty_file = vim.api.nvim_get_option_value(
-        "buftype", { buf = 0 }) == "nofile"
-      if ((args.event == "User" and args.file == "AlphaReady") or
-            (args.event == "BufEnter" and is_filetype_alpha)) and
-          not vim.g.before_alpha
+        "buftype",
+        { buf = 0 }
+      ) == "nofile"
+      if
+          (
+            (args.event == "User" and args.file == "AlphaReady")
+            or (args.event == "BufEnter" and is_filetype_alpha)
+          ) and not vim.g.before_alpha
       then
         vim.g.before_alpha = {
           showtabline = vim.opt.showtabline:get(),
-          laststatus = vim.opt.laststatus:get()
+          laststatus = vim.opt.laststatus:get(),
         }
         vim.opt.showtabline, vim.opt.laststatus = 0, 0
       elseif
@@ -145,7 +154,8 @@ if is_available "alpha-nvim" then
         return
       end
       for _, arg in pairs(vim.v.argv) do
-        if arg == "-b"
+        if
+            arg == "-b"
             or arg == "-c"
             or vim.startswith(arg, "+")
             or arg == "-S"
@@ -156,24 +166,24 @@ if is_available "alpha-nvim" then
 
       -- All good? Show alpha.
       require("alpha").start(true, require("alpha").default_config)
-      vim.schedule(function() vim.cmd.doautocmd "FileType" end)
+      vim.schedule(function() vim.cmd.doautocmd("FileType") end)
     end,
   })
 end
 
 -- 4. Update neotree when closing the git client.
-if is_available "neo-tree.nvim" then
+if is_available("neo-tree.nvim") then
   autocmd("TermClose", {
     pattern = { "*lazygit", "*gitui" },
     desc = "Refresh Neo-Tree git when closing lazygit/gitui",
     callback = function()
       local manager_avail, manager = pcall(require, "neo-tree.sources.manager")
       if manager_avail then
-        for _, source in ipairs {
+        for _, source in ipairs({
           "filesystem",
           "git_status",
           "document_symbols",
-        } do
+        }) do
           local module = "neo-tree.sources." .. source
           if package.loaded[module] then
             manager.refresh(require(module).name)
@@ -192,13 +202,17 @@ autocmd("BufWritePre", {
         and vim.bo[args.buf].buflisted
 
     if buf_is_valid_and_listed then
-      vim.fn.mkdir(vim.fn.fnamemodify(
-        vim.uv.fs_realpath(args.match) or args.match, ":p:h"), "p")
+      vim.fn.mkdir(
+        vim.fn.fnamemodify(
+          vim.uv.fs_realpath(args.match) or args.match,
+          ":p:h"
+        ),
+        "p"
+      )
     end
   end,
 })
 
--- 6. Auto-format and organize imports for Dart files.
 -- 6. Auto-format and organize imports for Dart files.
 autocmd("BufWritePre", {
   pattern = "*.dart",
@@ -218,9 +232,16 @@ autocmd("BufWritePre", {
   end,
 })
 
+-- Trigger Flutter hot reload on save for Dart files
+autocmd("BufWritePost", {
+  pattern = "*.dart",
+  desc = "Trigger Flutter hot reload on Dart file save",
+  command = "silent! !kill -s SIGUSR1 $(pgrep -f 'flutter_tool.*run')",
+})
+
 -- ## COOL HACKS ------------------------------------------------------------
 -- 6. Effect: URL underline.
-vim.api.nvim_set_hl(0, 'HighlightURL', { underline = true })
+vim.api.nvim_set_hl(0, "HighlightURL", { underline = true })
 autocmd({ "VimEnter", "FileType", "BufEnter", "WinEnter" }, {
   desc = "URL Highlighting",
   callback = function() utils.set_url_effect() end,
@@ -231,16 +252,20 @@ autocmd("VimEnter", {
   desc = "Disable right contextual menu warning message",
   callback = function()
     -- Revome from menu
-    vim.api.nvim_command [[aunmenu PopUp.How-to\ disable\ mouse]]
-    vim.api.nvim_command [[aunmenu PopUp.Inspect]]
-    vim.api.nvim_command [[aunmenu PopUp.-1-]] -- You can remove a separator like this.
+    vim.api.nvim_command([[aunmenu PopUp.How-to\ disable\ mouse]])
+    vim.api.nvim_command([[aunmenu PopUp.Inspect]])
+    vim.api.nvim_command([[aunmenu PopUp.-1-]]) -- You can remove a separator like this.
 
     -- Add to menu
-    vim.api.nvim_command [[menu PopUp.Format\ \Code <cmd>silent! Format<CR>]]
-    vim.api.nvim_command [[menu PopUp.-1- <Nop>]]
-    vim.api.nvim_command [[menu PopUp.Toggle\ \Breakpoint <cmd>:lua require('dap').toggle_breakpoint()<CR>]]
-    vim.api.nvim_command [[menu PopUp.Debugger\ \Continue <cmd>:DapContinue<CR>]]
-    vim.api.nvim_command [[menu PopUp.Run\ \Test <cmd>:Neotest run<CR>]]
+    vim.api.nvim_command([[menu PopUp.Format\ \Code <cmd>silent! Format<CR>]])
+    vim.api.nvim_command([[menu PopUp.-1- <Nop>]])
+    vim.api.nvim_command(
+      [[menu PopUp.Toggle\ \Breakpoint <cmd>:lua require('dap').toggle_breakpoint()<CR>]]
+    )
+    vim.api.nvim_command(
+      [[menu PopUp.Debugger\ \Continue <cmd>:DapContinue<CR>]]
+    )
+    vim.api.nvim_command([[menu PopUp.Run\ \Test <cmd>:Neotest run<CR>]])
   end,
 })
 
@@ -300,11 +325,15 @@ cmd("Swd", function()
 end, { desc = "cd current file's directory" })
 
 -- Write all buffers
-cmd("WriteAllBuffers", function()
-  vim.cmd("wa")
-end, { desc = "Write all changed buffers" })
+cmd(
+  "WriteAllBuffers",
+  function() vim.cmd("wa") end,
+  { desc = "Write all changed buffers" }
+)
 
 -- Close all notifications
-cmd("CloseNotifications", function()
-  require("notify").dismiss({ pending = true, silent = true })
-end, { desc = "Dismiss all notifications" })
+cmd(
+  "CloseNotifications",
+  function() require("notify").dismiss({ pending = true, silent = true }) end,
+  { desc = "Dismiss all notifications" }
+)
